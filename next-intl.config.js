f6a55f03e8908248c24cdc780f4ddb423fe6d592
Csaba Polyak <csiberius@gmail.com>
2025-07-23 21:32:46 +0200

    fix: rename next-intl config

--- next-intl.config.js
+++ next-intl.config.js
@@ -1,7 +1,7 @@
-import { defaultLocale, locales } from './i18n'
+import { defaultLocale, locales } from './i18n';
 
 export default {
   locales,
   defaultLocale,
   localePrefix: 'as-needed'
-} as const
+};
