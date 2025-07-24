e0832f99344d8ba8e5b17d64479585512ff94b43
Csaba Polyak <csiberius@gmail.com>
2025-07-24 06:06:28 +0200

    refactor i18n config

---
+++ next-intl.config.js
@@ -0,0 +1,8 @@
+export const locales = ['hu', 'en'];
+export const defaultLocale = 'en';
+
+export default {
+  locales,
+  defaultLocale,
+  localePrefix: 'as-needed'
+};
--- next-intl.config.ts
+++
@@ -1,7 +0,0 @@
-import { defaultLocale, locales } from './i18n'
-
-export default {
-  locales,
-  defaultLocale,
-  localePrefix: 'as-needed'
-} as const
